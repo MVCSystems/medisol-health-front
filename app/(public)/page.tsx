0b14349c2a13e476fc165e79a1566e27e01ebd33
MVCSystems <carlosmerav@gmail.com>
2025-10-08 03:54:24 -0500

    Fix errors dashboard

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/landing/hero";
 import AccesoRapido from "@/components/landing/quick-access";
-import RecentPublications from "@/components/landing/recent-publications";
 
 export default function Page() {
   return (
