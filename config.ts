475d494c22b7ed17ed3b8e8eb08e961ea31f601a
MVCSystems <carlosmerav@gmail.com>
2025-10-25 20:21:35 -0500

    Add new env

--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 export const siteConfig = {
-  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://medisol-health.onrender.com",
+  // backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://medisol-health.onrender.com",
+  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://medisol-back.fly.dev",
 };
