80a85c65e73aa6f08297a81b8769a661176e6264
MVCSystems <145077691+MVCSystems@users.noreply.github.com>
2025-10-29 13:38:10 -0500

    Update backend URL in siteConfig
    Update backend URL in siteConfig

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 export const siteConfig = {
   // backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://medisol-health.onrender.com",
-  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://170.81.242.107:9091",
+  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://ntechs.net.pe:9091",
 };
+
