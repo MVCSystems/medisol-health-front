484456ee5df948a760cc87814411ebdcc3812d23
MVCSystems <145077691+MVCSystems@users.noreply.github.com>
2025-10-30 00:05:49 -0500

    Fix siteConfig backend_url formatting
    Fix siteConfig backend_url formatting

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
   // backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://medisol-health.onrender.com",
-  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://apis.ntechs.net.pe/medisol/",
+  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL || "https://apis.ntechs.net.pe/medisol",
 };
+
 
 
